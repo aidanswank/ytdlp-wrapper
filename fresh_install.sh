c71c6d81c97be8be27fe381a16f156d1d32a3a22
aidan <aidan.c.swank@gmail.com>
2025-01-13 02:34:18 -0700

    install script

---
+++ fresh_install.sh
@@ -0,0 +1,7 @@
+#install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+#install node
+nvm install v18.17.0
+#install ytdlp
+sudo apt-get install yt-dlp
